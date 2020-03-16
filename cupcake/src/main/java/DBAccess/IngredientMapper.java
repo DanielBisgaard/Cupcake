bcdd7b7aea45318c77e94fb6a16de374dabd0e00
AlbertSylv <basketasl@hotmail.com>
2020-03-16 11:19:12 +0100

    new stuff
    Albert commit - start på lavning af java classes

---
+++ cupcake/src/main/java/DBAccess/IngredientMapper.java
@@ -0,0 +1,75 @@
+package DBAccess;
+
+import FunctionLayer.Top;
+import FunctionLayer.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class IngredientMapper {
+    public static ArrayList getTopIngredients() {
+        ArrayList<Top> topIngredients = new ArrayList<Top>();
+        try {
+            Connection con = Connector.connection();
+            String SQL = "SELECT * FROM OlskerCupCakes.top";
+
+
+            PreparedStatement ps = con.prepareStatement( SQL );
+
+
+            ResultSet rs = ps.executeQuery();
+
+            while ( rs.next() ) {
+                String ingredient = rs.getString("ingredient");
+                String email = rs.getString("email");
+                String password = rs.getString("password");
+                int price = rs.getInt("price");
+                int topid = rs.getInt("topid");
+                Top top = new Top(ingredient, price);
+                top.setId(topid);
+
+
+                topIngredients.add(top);
+            }
+
+
+        } catch ( ClassNotFoundException | SQLException ex ) {
+
+        }
+        return topIngredients;
+    }
+    public static ArrayList getBopIngredients() {
+        ArrayList<Top> topIngredients = new ArrayList<Top>();
+        try {
+            Connection con = Connector.connection();
+            String SQL = "SELECT * FROM OlskerCupCakes.bot";
+
+
+            PreparedStatement ps = con.prepareStatement( SQL );
+
+
+            ResultSet rs = ps.executeQuery();
+
+            while ( rs.next() ) {
+                String ingredient = rs.getString("ingredient");
+                String email = rs.getString("email");
+                String password = rs.getString("password");
+                int price = rs.getInt("price");
+                int topid = rs.getInt("topid");
+                Top top = new Top(ingredient, price);
+                top.setId(topid);
+
+
+                topIngredients.add(top);
+            }
+
+
+        } catch ( ClassNotFoundException | SQLException ex ) {
+
+        }
+        return topIngredients;
+    }
+}
---
+++ cupcake/src/main/java/PresentationLayer/Overview.java
@@ -0,0 +1,25 @@
+package PresentationLayer;
+
+import DBAccess.UserMapper;
+import FunctionLayer.LogicFacade;
+import FunctionLayer.LoginSampleException;
+import FunctionLayer.User;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+
+public class Overview extends Command{
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
+        String html = "overview";
+
+        HttpSession session = request.getSession();
+        ArrayList<User> brugere = UserMapper.getUsers();
+
+
+        session.setAttribute( "xy", brugere );
+        return html;
+    }
+
+}
